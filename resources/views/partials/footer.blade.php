be05639fc57dd50f27e1a1fc506bed9354ad9e83
Farabi Putra <bisonoalfarabi@gmail.com>
2026-01-18 10:58:55 +0700

    tambah foother

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,35 @@
+<footer class="footer">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4 mb-4">
+                <h5>FotoRental</h5>
+                <p>Penyewaan alat fotografi terpercaya dengan kualitas terbaik.</p>
+                <div class="social-links">
+                    <a href="#" class="text-white me-3"><i class="fab fa-facebook"></i></a>
+                    <a href="#" class="text-white me-3"><i class="fab fa-instagram"></i></a>
+                    <a href="#" class="text-white"><i class="fab fa-twitter"></i></a>
+                </div>
+            </div>
+            <div class="col-md-4 mb-4">
+                <h5>Quick Links</h5>
+                <ul class="list-unstyled">
+                    <li><a href="{{ route('home') }}" class="text-white">Beranda</a></li>
+                    <li><a href="{{ route('catalog.index') }}" class="text-white">Katalog</a></li>
+                    <li><a href="{{ route('home') }}#about" class="text-white">Tentang Kami</a></li>
+                    <li><a href="{{ route('home') }}#contact" class="text-white">Kontak</a></li>
+                </ul>
+            </div>
+            <div class="col-md-4 mb-4">
+                <h5>Kontak</h5>
+                <ul class="list-unstyled">
+                    <li><i class="fas fa-map-marker-alt me-2"></i>Jl. Fotografi No. 123, Bandung</li>
+                    <li><i class="fas fa-phone me-2"></i>+62 812 3456 7890</li>
+                    <li><i class="fas fa-envelope me-2"></i>info@fotorental.com</li>
+                </ul>
+            </div>
+        </div>
+        <div class="text-center mt-4 pt-4 border-top">
+            <p>&copy; 2024 FotoRental. All rights reserved.</p>
+        </div>
+    </div>
+</footer>
